a3ad673fa0b026aae3ae3793bd3cd2315441a3d4
KP-Zhang <Dylan-cheung@hotmail.com>
2021-01-16 16:51:51 +0800

    Update model.py

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class Model(nn.Module):
             GATBlock(input_dim, hidden_dim, stride=1, residual=True),
         ))
 
-        self.EncoderDecoder = EncoderDecoder(input_dim=input_dim,
+        self.encoder_decoder = EncoderDecoder(input_dim=input_dim,
                                              hidden_dim=hidden_dim,
                                              output_dim=output_dim,
                                              node_number=node_number)
@@ -28,7 +28,7 @@ class Model(nn.Module):
         graph_feature = feature.permute(0, 2, 1, 3).unsqueeze(2).contiguous()
         last_location = agent_feature.unsqueeze(1).permute(0, 1, 3, 4, 2).contiguous()[:, :, -1:]
 
-        predicted_xy = self.EncoderDecoder(in_data=graph_feature,
+        predicted_xy = self.encoder_decoder(in_data=graph_feature,
                                            last_location=last_location,
                                            steps=steps)
         predicted_xy = predicted_xy.permute(0, 2, 1, 3).contiguous()
